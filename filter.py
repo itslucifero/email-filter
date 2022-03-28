2025da5c81065e421fc8841db0986a76de6d7a9b
its Yahia <85070360+hrackedz@users.noreply.github.com>
2022-03-28 14:53:30 +0200

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,62 @@
+import sys
+import os
+from time import sleep
+print("""           Email Filter By Domain v 0.0.1
+
+ ██░ ██  ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████ ▓█████▄ ▒███████▒
+▓██░ ██▒▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▒██▀ ██▌▒ ▒ ▒ ▄▀░
+▒██▀▀██░▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ░██   █▌░ ▒ ▄▀▒░ 
+░▓█ ░██ ▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ░▓█▄   ▌  ▄▀▒   ░
+░▓█▒░██▓░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░▒████▓ ▒███████▒
+ ▒ ░░▒░▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░ ▒▒▓  ▒ ░▒▒ ▓░▒░▒
+ ▒ ░▒░ ░  ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░ ░ ▒  ▒ ░░▒ ▒ ░ ▒
+ ░  ░░ ░  ░░   ░   ░   ▒   ░        ░ ░░ ░    ░    ░ ░  ░ ░ ░ ░ ░ ░
+ ░  ░  ░   ░           ░  ░░ ░      ░  ░      ░  ░   ░      ░ ░    
+                           ░                       ░      ░        
+                            
+                    Telegram : https://t.me/kickflap
+                    Website : https://hrackedz.com        
+                            
+                            
+                             """) 
+print(' GIVE ME A FRACTION OF SECOND MUHAHAHAH\n')
+print('RESULT WILL BE SAVED IN HRACKEDZ REZULT FOLDER.\n \n')                                          
+mlist = input("Enter the list path : ")
+
+dir = 'HRACKEDZ REZULT'
+os.mkdir(dir)
+
+for email in open(mlist, 'r'):
+    email = email.strip()
+    if '@gmail' in email:
+        gmail = open(f'{dir}/gmail.txt', 'a+')
+        gmail.write(email + '\n')
+        gmail.close()
+        print('[GMAIL] -', email)
+    elif '@yahoo' in email:
+        yahoo = open(f'{dir}/yahoo.txt', 'a+')
+        yahoo.write(email + '\n')
+        yahoo.close()
+        print('[YAHOO] -', email)
+    elif '@hotmail' in email:
+        hotmail = open(f'{dir}/hotmail.txt', 'a+')
+        hotmail.write(email + '\n')
+        hotmail.close()
+        print('[HOTMAIL] -', email)
+    elif '@aol.com' in email:
+        hotmail = open(f'{dir}/aol.txt', 'a+')
+        hotmail.write(email + '\n')
+        hotmail.close()
+        print('[AOL] -', email)
+    elif '@yandex.ru' in email:
+        hotmail = open(f'{dir}/yandex-ru.txt', 'a+')
+        hotmail.write(email + '\n')
+        hotmail.close()
+        print('[YANDEX] -', email)
+    else:
+        other = open(f'{dir}/other.txt', 'a+')
+        other.write(email + '\n')
+        other.close()
+print("-------------------------------")
+print("IM DONE GETFO!!!!! ", len(open(mlist, 'r').readlines()), ' ,i,')
+sleep(2)
